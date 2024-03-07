a7805d71cca57620bc9b372bd74859144b858939
ibrahim <ibrahim@addissoftware.com>
2024-03-07 21:37:02 +0300

    ft: done connecting with typeorm.

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { TodosController } from './todos.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoEntity } from './entities/todo.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TodoEntity])],
   controllers: [TodosController],
   providers: [TodosService],
 })
